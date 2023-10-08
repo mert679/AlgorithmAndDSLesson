3679e3ee9bb1b6561ab6f30b38c887a180d3cbd9
mert679 <mertaltintas78@hotmail.com>
2023-10-08 22:18:14 +0300

    Proje dosyası ekle.

---
+++ AlgorithmAndDSLesson/Node.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmAndDSLesson
+{
+   public class Node
+    {
+        public int value;
+        public Node next;
+        public Node(int value) {
+            this.value = value;
+            this.next = null;
+        }
+        
+   }
+    
+   public class LinkedList
+    {
+        public Node Head;
+        public Node Tail;
+        public LinkedList() {
+            this.Head = null;
+            this.Tail = null;
+        }
+        // add element end of list
+        public void AddElement(int data)
+        {
+            Node node = new Node(data);
+
+            if(Head== null)
+            {
+                Head = node;
+            }
+            else
+            {
+                Tail.next = node;
+            }
+            Tail = node;
+            
+        }
+        // find length of list
+        public int findLength()
+        {
+            var tmp = Head;
+            var count = 0;
+            while(tmp.next != null) {
+                //visit next node
+                tmp = tmp.next;
+                count++;
+            }
+            return count;
+        }
+        // Display all Linked List elements
+        public void display()
+        {
+            if (this.Head != null)
+            {
+                Console.Write("Linked List Element :");
+                var temp = this.Head;
+                while (temp != null)
+                {
+                    Console.Write("  " + temp.value);
+                    temp = temp.next;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Empty Linked list");
+            }
+        }
+    }
+}
---
+++ AlgorithmAndDSLesson/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+using AlgorithmAndDSLesson;
+using System;
+
+var task = new LinkedList();
+task.AddElement(5);
+task.AddElement(10);
+task.AddElement(15);
+task.AddElement(20);
+task.AddElement(25);
+task.AddElement(30);
+task.AddElement(35);
+task.display();
+Console.WriteLine("\nLength :  " + task.findLength());
